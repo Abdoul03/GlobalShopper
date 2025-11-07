91799ee75b183b9e5e023ce76209ef67cbb1d110
Abdoul03 <samabdoul03@gmail.com>
2025-11-07 11:33:14 +0000

    preparation pour la production

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM openjdk:17-jdk-slim
+FROM eclipse-temurin:17-jdk
 
 WORKDIR /app
 
 COPY /app/target/*.jar globalShopper.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "globalShopper.jar"]
+ENTRYPOINT ["java", "-jar", "globalShopper.jar --server.port=${PORT:-8080}"]
--- src/main/java/com/globalshopper/GlobalShopper/GlobalShopperApplication.java
+++ src/main/java/com/globalshopper/GlobalShopper/GlobalShopperApplication.java
@@ -2,8 +2,10 @@ package com.globalshopper.GlobalShopper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GlobalShopperApplication {
 
 	public static void main(String[] args) {
