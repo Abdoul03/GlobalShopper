7d5a2fa385cd501d14d873fc35047d6900d9ca39
Abdoul03 <samabdoul03@gmail.com>
2025-11-27 10:27:02 +0000

    ajout de cronjob

--- src/main/java/com/globalshopper/GlobalShopper/config/SecurityConfig.java
+++ src/main/java/com/globalshopper/GlobalShopper/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                             auth.requestMatchers("/auth/**").permitAll()
                                     .requestMatchers("/users/me/**").permitAll()
+                                    .requestMatchers("/about").permitAll()
                                     .requestMatchers("/swagger-ui/**").permitAll()
                                     .requestMatchers("/swagger-ui.html/**").permitAll()
                                     .requestMatchers("/v3/api-docs/swagger-config/**").permitAll()
